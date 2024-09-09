75a14efce4674cf337a01af98563d1353fbc659e
Clyde Cash <135058512+ClydeCash@users.noreply.github.com>
2024-09-09 17:43:58 -0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,10 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"dwmblock-calendar",		1,			2		},
+	{"",	"dwmblock-clock",		1,			1		},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " ";
+

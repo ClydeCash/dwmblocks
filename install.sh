185831263783c263e31f7af04b728e9d919d3560
Clyde Cash <135058512+ClydeCash@users.noreply.github.com>
2024-09-25 03:23:47 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 cd ~/dwmblocks &&
 sudo make install &&
+mkdir -p ~/.cache/statusbar &&
 cd ~/dwmblocks/blocks &&
 sudo cp -f dwmblock-calendar dwmblock-clock dwmblock-cpu-temp dwmblock-gpu-temp-nvidia dwmblock-network dwmblock-volume dwmblock-weather-report weather-report /bin &&
 cd ~/ &&
- sudo rm -r ~/dwmblocks &&
+sudo rm -r ~/dwmblocks &&
 cd /bin
 sudo chmod +x dwmblock-calendar dwmblock-clock dwmblock-cpu-temp dwmblock-gpu-temp-nvidia dwmblock-network dwmblock-volume dwmblock-weather-report weather-report &&
 echo "All done." && exit

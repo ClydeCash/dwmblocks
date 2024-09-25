019db184d7ba79e8843fc91d86b383c27b9dfc00
Clyde Cash <135058512+ClydeCash@users.noreply.github.com>
2024-09-25 04:00:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ sudo make install &&
 mkdir -p ~/.cache/statusbar &&
 cd ~/dwmblocks/blocks &&
 sudo cp -f dwmblock-calendar dwmblock-clock dwmblock-cpu-temp dwmblock-gpu-temp-nvidia dwmblock-network dwmblock-volume dwmblock-weather-report weather-report /bin &&
-cd ~/ &&
-sudo rm -r ~/dwmblocks &&
-cd /bin
+cd /bin &&
 sudo chmod +x dwmblock-calendar dwmblock-clock dwmblock-cpu-temp dwmblock-gpu-temp-nvidia dwmblock-network dwmblock-volume dwmblock-weather-report weather-report &&
 echo "All done." && exit

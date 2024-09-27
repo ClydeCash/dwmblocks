337569b8515240d7f7653a6caeb554d208fc3890
Clyde Cash <135058512+ClydeCash@users.noreply.github.com>
2024-09-27 08:14:00 -0300

    Add files via upload

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"dwmblock-gpu-temp-nvidia",	5,			8		},
-	{"",	"dwmblock-cpu-temp",		5,			7		},
+	{"",	"dwmblock-gpu-temp-nvidia",	5,			9		},
+	{"",	"dwmblock-cpu-temp",		5,			8		},
+	{"",	"dwmblock-storage",		10,			7		},
 	{"",	"dwmblock-network",		10,			6		},
 	{"",	"dwmblock-volume",		0,			5		},
 	{"",	"dwmblock-moon-report",		21600,			4		},
